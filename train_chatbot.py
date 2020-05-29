de07852d6ada0ab9290af0a230413bf09b3089c5
mirandadayadkins <mirandaday16@gmail.com>
2020-05-29 15:34:12 -0400

    creates training data lists

--- train_chatbot.py
+++ train_chatbot.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
-nltk.download('wordnet')
+# nltk.download('wordnet')
 import json
 import pickle
 import tensorflow
@@ -41,3 +41,35 @@ classes = sorted(list(set(classes)))
 # Save words and classes to pickle files
 pickle.dump(words, open("python-project-chatbot-codes/words.pkl", 'wb'))
 pickle.dump(classes, open('python-project-chatbot-codes/classes.pkl', 'wb'))
+
+# Create training data
+training = []
+# Empty list for output
+output_empty = [0] * len(classes)
+# "Bag of Words" for each sentence
+for document in documents:
+    bag_of_words = []
+    # list of tokenized words in the pattern
+    pattern_words = document[0]
+    # lemmatize pattern words
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    # Append 1 to bag of words if word match is found in current pattern, otherwise 0
+    for word in words:
+        if word in pattern_words:
+            bag_of_words.append(1)
+        else:
+            bag_of_words.append(0)
+    # Output is 0 for each tag and 1 for current tag
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag_of_words, output_row])
+
+# Shuffle features to create numpy array
+random.shuffle(training)
+training = np.array(training)
+
+# Create training and test sets
+train_patterns = list(training[:,0])
+train_intents = list(training[:,1])
+
+print("Training data created.")
